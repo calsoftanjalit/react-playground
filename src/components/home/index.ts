2009575ca03a169d5f45f0943ad54ac3d0f5eeaa
Rakesh Singh Kushwah <rakesh.kushwah@calsoftinc.com>
2025-11-07 13:33:39 +0530

    Add loader in index.ts file

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -2,3 +2,4 @@ export * from './HeroSection';
 export * from './FeaturedCategories';
 export * from './PopularProducts';
 export * from '../miscellaneous/Footer';
+export * from '../miscellaneous/Loader'
