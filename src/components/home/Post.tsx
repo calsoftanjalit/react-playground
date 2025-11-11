c499a9551855adc703e7bbc5cc9095506ca602b3
Rakesh Singh Kushwah <rakesh.kushwah@calsoftinc.com>
2025-11-11 13:07:03 +0530

    remove type keyword from Post

--- src/components/home/Post.tsx
+++ src/components/home/Post.tsx
@@ -1,22 +1,19 @@
-import { Card, Text } from "@mantine/core";
-import type { PostInterface } from "@types";
+import { Card, Text } from '@mantine/core';
+import { PostInterface } from '@types';
 
-const Post: React.FC<PostInterface> = ({
-  id,
-  title,
-  body
-}) => {
-
-  return(
+const Post: React.FC<PostInterface> = ({ id, title, body }) => {
+  return (
     <>
-    <Card key={id} shadow="sm" padding="md" radius="md" withBorder>
-      <Text fw={600} tt="capitalize">{title}</Text>
-      <Text size="sm" mt="xs">
-        {body}
-      </Text>
-    </Card>
+      <Card key={id} shadow="sm" padding="md" radius="md" withBorder>
+        <Text fw={600} tt="capitalize">
+          {title}
+        </Text>
+        <Text size="sm" mt="xs">
+          {body}
+        </Text>
+      </Card>
     </>
-  )
-}
+  );
+};
 
 export default Post;
