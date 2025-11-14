08aadf909bcd516dd73b29a9d62ad22bd54e58eb
sameeksha-sihare-calsoft <sameeksha.sihare@calsoftinc.com>
2025-11-14 14:32:34 +0530

    Add common header component

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,5 @@
+export { Header } from './Header';
+export { HeaderActions } from './HeaderActions';
 export { Layout } from './Layout';
 export { Navbar } from './Navbar';
 export { UserMenu } from './UserMenu';
